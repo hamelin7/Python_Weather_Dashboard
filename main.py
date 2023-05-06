11ff116e5f4f44b6b6b86a8271bcd9bfe2900766
Greg Hamelin <132278697+hamelin7@users.noreply.github.com>
2023-05-06 14:35:50 -0400

    1.12
    had to change time.sleep() back to 20 seconds due to throttling error from adafruit.io

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import OpenWeather_data
 
 #use while loop to continuously check the 'zip-code-feed'
 #set time.sleep to 15 seconds to not exceed the 30 data points per minute rate limit set by adafruit.io free API.
-#we are sending 7 data points of weather data to our adafruit.io dashboard so we can send those 7 data points every 15 seconds without risking exceeding the rate limit
+#we are sending 7 data points of weather data to our adafruit.io dashboard so we can send those 7 data points every 20 seconds without risking exceeding the rate limit
 
 def main():
   while True:
@@ -20,8 +20,8 @@ def main():
     weatherData = OpenWeather_data.getWeatherData(zipCode)
     #pass currentWeather and forecastWeather data to adafruit_data.py to be sent to our dashboard
     adafruit_data.postData(weatherData)
-    
-    time.sleep(15)
+    #wait 20 seconds so we're not posting more than 30 data points per minute to our Adafruit.io dashboard.
+    time.sleep(20)
 #end main 
 
 main()
